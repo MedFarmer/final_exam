05a196cc6d38e954c27684dc93b8761ccf30a14f
Medet <m.shamuratov@gmail.com>
2023-12-15 16:46:34 +0600

    wsgi updated

--- board/wsgi.py
+++ board/wsgi.py
@@ -1,11 +1,3 @@
-"""
-WSGI config for board project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
 
 import os
 
@@ -14,3 +6,13 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'board.settings')
 
 application = get_wsgi_application()
+
+
+# Custom server setup (if not using Gunicorn)
+from django.core.servers.basehttp import get_internal_wsgi_application
+import os
+
+if __name__ == "__main__":
+    from django.core.management.commands.runserver import Command as runserver
+    runserver.default_port = os.environ.get('PORT', '8000')
+
